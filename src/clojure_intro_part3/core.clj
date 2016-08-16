27e6fde6d5d9e0152476130172f7be37e98e6380
Jorge Gomez <jorgemario@belucasys.com>
2016-08-15 20:54:21 -0500

    clean up

--- src/clojure_intro_part3/core.clj
+++ src/clojure_intro_part3/core.clj
@@ -5,64 +5,20 @@
   (apply println rs)
   :error)
 
-(defn add-egg []
-  (grab :egg)
-  (squeeze)
-  (add-to-bowl))
-
-(defn add-sugar []
-  (grab :cup)
-  (scoop :sugar)
-  (add-to-bowl)
-  (release))
-
-(defn add-flour []
-  (grab :cup)
-  (scoop :flour)
-  (add-to-bowl)
-  (release))
-
-(defn add-milk []
-  (grab :cup)
-  (scoop :milk)
-  (add-to-bowl)
-  (release))
-
-(defn add-butter []
-  (grab :butter)
-  (add-to-bowl))
-
 (def scooped-ingredients #{:milk :sugar :flour})
+
 (defn scooped? [ingredient]
   (contains? scooped-ingredients ingredient))
 
 (def squeezed-ingredients #{:egg})
+
 (defn squeezed? [ingredient]
   (contains? squeezed-ingredients ingredient))
 
 (def simple-ingredients #{:butter})
+
 (defn simple? [ingredient]
   (contains? simple-ingredients ingredient))
-
-(defn add-eggs [n]
-  (dotimes [e n]
-    (add-egg)))
-
-(defn add-flour-cups [n]
-  (dotimes [e n]
-    (add-flour)))
-
-(defn add-milk-cups [n]
-  (dotimes [e n]
-    (add-milk)))
-
-(defn add-sugar-cups [n]
-  (dotimes [e n]
-    (add-sugar)))
-
-(defn add-butters [n]
-  (dotimes [e n]
-    (add-butter)))
 
 (defn add-squeezed
   ([ingredient]
@@ -140,10 +96,12 @@
   (cool-pan))
 
 (def pantry-ingredients #{:sugar :flour})
+
 (defn from-pantry? [ingredient]
   (contains? pantry-ingredients ingredient))
 
 (def fridge-ingredients #{:egg :milk :butter})
+
 (defn from-fridge? [ingredient]
   (contains? fridge-ingredients ingredient))
 
